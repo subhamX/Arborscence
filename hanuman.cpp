6c404e3724815a02412464ff9d6412ed06c41978
subham <subham.iitrpr@gmail.com>
2019-11-12 19:13:41 +0530

    Adds Strongly Connected Components Algo

--- hanuman.cpp
+++ hanuman.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ struct node{
 
 struct point{
     node data;
+    bool visible;
     point* next;
 };
 
@@ -17,6 +19,7 @@ point* createNode(node payload){
     // Can use keyword new too
     point* newNode = (point*)malloc(sizeof(point));
     newNode->data = payload;
+    newNode->visible = true;
     newNode->next = NULL;
     return newNode;
 }
@@ -80,11 +83,66 @@ bool isPresent(point* head, int index){
     return false;
 }
 
+
+// STACK FUNCTIONS START
+struct Stack{
+    int data;
+    Stack* next;
+};
+
+Stack* insertStackNode(Stack* top, int data){
+    if(top==NULL){
+        Stack* temp = (Stack*)malloc(sizeof(Stack));
+        temp->data = data;
+        temp->next=NULL;
+        return temp;
+    }
+    Stack* temp = insertStackNode(NULL, data);
+    temp->next = top;
+    return temp;
+}
+
+Stack* deleteStackNode(Stack*top){
+    if(top==NULL){
+        return NULL;
+    }else if(top->next==NULL){
+        free(top);
+        return NULL;
+    }
+    Stack* t = top->next;
+    free(top);
+    return t;
+}
+bool isStackEmpty(Stack* top){
+    if(top==NULL){
+        return true;
+    }
+    return false;
+}
+int getStackTop(Stack* top){
+    if(top==NULL){
+        return INT_MIN;
+    }
+    return top->data;
+}
+
+// STACK FUNCTIONS END
+
+
+
+
+
 void deallocate(point* head){
     while(head){
         point* t1 = head->next;
         free(head);
         head = t1;
+    }
+}
+
+void deallocateDriver(vector<point*> &Adj, int n){
+    for(int i=0; i<n; i++){
+        deallocate(Adj[i]);
     }
 }
 
@@ -96,11 +154,115 @@ void traversePoints(point* head){
     cout<<endl;
 }
 
+void dfs(vector<point*> &Adj, int n, Stack* &top, int* visited, int start){
+    visited[start] = 1;
+    point* head = Adj[start];
+    while(head){
+        // Checking if the 
+        if(head->visible){
+            int vertex = head->data.vertex;
+            if(visited[vertex]==0){
+                dfs(Adj, n, top, visited, vertex);
+                // cout<<"INSERTING"<<start<<endl;
+            }
+        }
+        head = head->next;
+    }
+    top = insertStackNode(top, start);
+}
+
+void dfs2(vector<point*> &AdjT, int n, int* visited, int start, vector<int> &comp){
+    visited[start] = 1;
+    point* head = AdjT[start];
+    comp.push_back(start);
+    while(head){
+        if(head->visible){
+            int vertex = head->data.vertex;
+            if(visited[vertex]==0){
+                dfs2(AdjT, n, visited, vertex, comp);
+            }
+        }
+        head= head->next;
+    }
+}
+void getSCC(vector<point*> &Adj, int n, vector< vector<int> > &comps){
+    vector<point*> AdjT(n);
+    point* t1;
+    node temp;
+    int source;
+    for(int i=0; i<n; i++){
+        t1 = Adj[i];
+        while(t1){
+            temp.index = t1->data.index;
+            temp.vertex = i;
+            temp.weight = t1->data.weight;
+            source = t1->data.vertex;
+            AdjT[source] = addNode(AdjT[source], temp);
+            t1 = t1->next;
+        }
+    }
+    int visited[n] = { 0 };
+    Stack* top = NULL;
+    for(int i=0; i<n; i++){
+        if(visited[i] == 0){
+            // Sending top of stack by reference
+            dfs(Adj, n, top, visited, i);
+        }
+    }
+    for(int i=0; i<n; i++){
+        visited[i] = 0;
+    }
+    vector<int> comp;
+    while(!isStackEmpty(top)){
+        int start = getStackTop(top);
+        top = deleteStackNode(top);
+        if(visited[start]==0){
+            dfs2(AdjT, n, visited, start, comp);
+            comps.push_back(comp);
+            comp.clear();
+        }
+    }
+}
+
+
+void driver(vector<point*> &Adj, int n, int start){
+    vector< vector<int> > comps;
+    getSCC(Adj, n, comps);
+
+
+    cout<<"PRINTING SCC:"<<endl;
+    for(int i=0; i<comps.size(); i++){
+        for(int j=0; j<comps[i].size(); j++){
+            cout<<comps[i][j]<<endl;
+        }
+        cout<<endl;
+    }
+}
+
+
 int main(){
     point* head = NULL;
-    node temp;
-    
+    node payload;
+    int t;
+    cin>>t;
+    int n, start, m, source, dest, wt;
+    while(t--){
+        cin>>n>>start>>m;
+        vector<point*> Adj(n, NULL);
+        for(int i=0; i<m; i++){
+            cin>>source>>dest>>wt;
+            source-=1;
+            dest-=1;
+            payload.index = i;
+            payload.vertex = dest;
+            payload.weight = wt;
+            Adj[source] = addNode(Adj[source], payload);
+        }
+        // TODO : Check if all nodes has atleast 1 indegree.(except start). If No print -1;
 
-    deallocate(head);
+        driver(Adj, n, start);
+        deallocateDriver(Adj, n);
+    }
+
     return 0;
 }
